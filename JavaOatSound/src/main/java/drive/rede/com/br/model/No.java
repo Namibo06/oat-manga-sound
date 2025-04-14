656f32787b93cc34c9bea5394dcb9e9aa17add4d
Namibo06 <namibo654@gmail.com>
2025-04-13 21:41:54 -0300

    feat: added content in class No, and class ListaEncadeada

--- JavaOatSound/src/main/java/drive/rede/com/br/model/No.java
+++ JavaOatSound/src/main/java/drive/rede/com/br/model/No.java
@@ -1,4 +1,30 @@
 package drive.rede.com.br.model;
 
 public class No {
+    private Object valor;
+    private No prox;
+
+    public No() {
+    }
+
+    public No(Object valor) {
+        this.valor = valor;
+        this.prox = null;
+    }
+
+    public Object getValor() {
+        return valor;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setValor(Object valor) {
+        this.valor = valor;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
 }
